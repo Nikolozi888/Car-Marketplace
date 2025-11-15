997a3266552c18ad32e9f10515f92cfe81fdb55f
Nikolozi888 <melashvilinikolozi@gmail.com>
2025-11-16 00:57:02 +0400

    Custom Middleware & Aliases

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\CarController;
+use App\Http\Middleware\AdminMiddleware;
 
 Route::get('/', function () {
     return redirect()->route('cars.index');
@@ -11,7 +12,7 @@ Route::get('/', function () {
 
 Route::resource('cars', CarController::class);
 
-Route::prefix('admin')->name('admin.')->group(function () {
+Route::prefix('admin')->name('admin.')->middleware('admin')->group(function () {
 
     Route::get('/', [AdminController::class, 'dashboard'])->name('dashboard');
     Route::get('/cars', [AdminController::class, 'carsIndex'])->name('cars.index');
