6a9cd1c80d1ce35fda9a362fe4f8bce1a211b050
Nikolozi888 <melashvilinikolozi@gmail.com>
2025-12-05 23:59:18 +0400

    add Custom Facade

---
+++ app/Facades/ImageManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class ImageManager extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        // code for image service call
+    }
+}
