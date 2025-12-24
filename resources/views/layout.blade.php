b1d0ecdeb89e4998080974c147a8d3f50b1faff6
Nikolozi888 <melashvilinikolozi@gmail.com>
2025-12-25 00:48:56 +0400

    Add Site icon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}"> <title>Car Project</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="icon" href="{{ asset('site_icon.jpg') }}" type="image/jpg">
+    <title>Car Project</title>
 
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
